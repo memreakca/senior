f397cf78234f918f6040f20dfddcc911387ebd13
Mehmet Emre Akca <96843001+memreakca@users.noreply.github.com>
2023-11-05 11:43:51 +0300

    temporary save,load

--- Assets/Scripts/MainChScripts/Player.cs
+++ Assets/Scripts/MainChScripts/Player.cs
@@ -16,6 +16,18 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            inventory.Save();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            inventory.Load();
+        }
+
+    }
     private void OnApplicationQuit()
     {
         inventory.Container.Clear();
